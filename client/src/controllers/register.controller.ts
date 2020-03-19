e5003522da7165dc320322b66d856010a7cc48cf
Michael Morgoun <mmo155@uclive.ac.nz>
2020-03-20 09:27:44 +1300

    Fixed grammar issues and applied password constraints to register.controller.ts.

--- client/src/controllers/register.controller.ts
+++ client/src/controllers/register.controller.ts
@@ -19,55 +19,55 @@ export interface RegisterFormData {
 export async function registerUser(formData: RegisterFormData) {
 
     if (!formData.lastName || formData.lastName.length < 1) {
-      throw new Error("no last name given")
+      throw new Error("No last name given")
     }
 
     if (formData.lastName.length > 30) {
-        throw new Error("last name must be less than 30 characters")
+        throw new Error("Last name must be less than 30 characters")
     }
 
     if (hasNumber(formData.lastName)) {
-        throw new Error("last name cannot contain numbers")
+        throw new Error("Last name cannot contain numbers")
     }
 
     if (!formData.firstName || formData.firstName.length < 1) {
-        throw new Error("no first name given")
+        throw new Error("No first name given")
     }
 
     if (formData.firstName.length > 30) {
-        throw new Error("first name must be less than 30 characters")
+        throw new Error("First name must be less than 30 characters")
     }
 
     if (hasNumber(formData.firstName)) {
-        throw new Error("first name cannot contain numbers")
+        throw new Error("First name cannot contain numbers")
     }
 
     if (formData.middleName && formData.middleName.length > 30) {
-        throw new Error("middle name must be less than 30 characters")
+        throw new Error("Middle name must be less than 30 characters")
     }
 
     if (formData.middleName && hasNumber(formData.middleName)) {
-        throw new Error("middle name cannot contain numbers")
+        throw new Error("Middle name cannot contain numbers")
     }
 
     if (formData.nickname && formData.nickname.length < 6) {
-        throw new Error("nick name must be at least 6 characters long")
+        throw new Error("Nick name must be at least 6 characters long")
     }
 
     if (formData.nickname && hasWhiteSpace(formData.nickname)) {
-        throw new Error("nickname cannot contain white space")
+        throw new Error("Nickname cannot contain white space")
     }
 
     if (!formData.email || !isValidEmail(formData.email)) {
-        throw new Error("invalid email address")
+        throw new Error("Invalid email address")
     }
 
     if (!formData.password) {
-        throw new Error("password cannot be empty")
+        throw new Error("Password cannot be empty")
     }
 
     if (formData.password.length < 8) {
-        throw new Error("password must be at least 8 characters")
+        throw new Error("Password must be at least 8 characters")
     }
 
     if (formData.bio && formData.bio.length < 8) {
@@ -75,24 +75,24 @@ export async function registerUser(formData: RegisterFormData) {
     }
 
     if (!formData.dateOfBirth) {
-        throw new Error("date of birth cannot be empty")
+        throw new Error("Date of birth cannot be empty")
     }
     // date of birth input format is YYYY-MM-DD
     const date = Date.parse(formData.dateOfBirth);
         if (isNaN(date)) {
-        throw new Error('valid date not given')
+        throw new Error('Valid date not given')
     }
 
     if (date > Date.now()) {
-        throw new Error("date of birth cannot be in the future")
+        throw new Error("Date of birth cannot be in the future")
     }
 
     if (!formData.gender) {
-        throw new Error("no gender given")
+        throw new Error("No gender given")
     }
     
     if (formData.password !== formData.confirmPassword) {
-      throw new Error("passwords do not match")
+      throw new Error("Passwords do not match")
     }
 
     await create(formData);
