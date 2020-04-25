c8f0f0f299995a117207ad2e3c7e2c51c09404d9
Jack van Heugten Breurkes <jack.breurkes@gmail.com>
2020-04-25 12:21:19 +1200

    implemented Activity JPA POJO
    - started trying to implement ActivityRepositoryTest, but weren't
    sure how to get the mocking working.
    
    #story[246] #pair[ras226, jsv22]

--- server/src/main/java/com/springvuegradle/model/data/Activity.java
+++ server/src/main/java/com/springvuegradle/model/data/Activity.java
@@ -1,19 +1,29 @@
 package com.springvuegradle.model.data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * JPA POJO representing an Activity.
+ * @author Jack van Heugten Breurkes
+ * @author Riley Symon
+ */
+@Entity
+@Table(name = "activity")
 public class Activity {
 
     @Id
     @GeneratedValue
     private long id;
 
-    @Column(columnDefinition = "varchar(30) not null")
+    @NotNull  // will automatically set column to not null if hibernate.validator.apply_to_ddl = true
+    @Column(columnDefinition = "varchar(30)")
     private String activityName;
 
-    @Column(columnDefinition = "boolean not null")
+    @NotNull
+    @Column(columnDefinition = "boolean")
     private boolean isDuration;
 
     @Column(columnDefinition = "date")
@@ -31,12 +41,21 @@ public class Activity {
     @Column(columnDefinition = "text")
     private String description;
 
-    @Column(columnDefinition = "varchar(30) not null")
+    @NotNull
+    @Column(columnDefinition = "varchar(30)")
     private String location;
 
-    @ManyToOne
+    @NotNull
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="creator_uuid")
     private Profile creator;
-    //TODO activity types
+
+    //TODO activity types once they have been added
+
+    /**
+     * no arg constructor required by JPA
+     */
+    protected Activity() {}
 
     /**
      * Create an activity with required and optional fields
@@ -51,6 +70,78 @@ public class Activity {
         this.isDuration = isDuration;
         this.location = location;
         this.creator = creator;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public boolean isDuration() {
+        return isDuration;
+    }
+
+    public void setIsDuration(boolean isDuration) {
+        this.isDuration = isDuration;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Profile getCreator() {
+        return creator;
     }
 
 }
