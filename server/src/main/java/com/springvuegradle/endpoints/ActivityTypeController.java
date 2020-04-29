16b0b9affd955454b749b6f32b414a43e3ae4b87
Alex Hobson <aho113@uclive.ac.nz>
2020-04-29 17:24:03 +1200

    Implement /activities endpoint which returns a list of activities string
    #story[244]

---
+++ server/src/main/java/com/springvuegradle/endpoints/ActivityTypeController.java
@@ -0,0 +1,52 @@
+package com.springvuegradle.endpoints;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springvuegradle.model.data.ActivityType;
+import com.springvuegradle.model.repository.ActivityTypeRepository;
+
+/**
+ * REST endpoint for getting a list of activity types
+ * 
+ * @author Alex Hobson
+ *
+ */
+@RestController
+public class ActivityTypeController {
+	
+	/**
+	 * Repository of activity types in the database
+	 */
+	@Autowired
+	private ActivityTypeRepository activityRepo;
+	
+	/**
+	 * REST Endpoint for GET /activities
+	 * Returns a response of a list of all the activity types as strings
+	 * 
+	 * @return Response entity containing list of all activity types in database
+	 */
+	@GetMapping("/activities")
+	@CrossOrigin
+	public ResponseEntity<?> getActivities() {
+		List<ActivityType> allActivityTypes = activityRepo.findAll();
+		
+		String[] activityTypeString = new String[allActivityTypes.size()];
+		
+		for (int i = 0; i < activityTypeString.length; i++) {
+			activityTypeString[i] = allActivityTypes.get(i).getActivityTypeName();
+		}
+		
+		return ResponseEntity.ok(activityTypeString);
+	}
+
+}
