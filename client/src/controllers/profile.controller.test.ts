787646439fbbf557af5e7877798689aded727dbb
Olivia Mackintosh <oam23@uclive.ac.nz>
2020-04-30 12:13:02 +1200

    Wrote tests for adding/removing activity types for profile.controller
    Wrote blue sky scenario and exception flow tests for the add/removeActivityType functions in the profile.controller file.
    
    #pair[mjf145] #story[244]

--- client/src/controllers/profile.controller.test.ts
+++ client/src/controllers/profile.controller.test.ts
@@ -1,4 +1,5 @@
-import { updatePassword } from './profile.controller'
+import { updatePassword, addActivityType, removeActivityType } from './profile.controller'
+import { UserApiFormat } from '@/scripts/User';
 
 const userModel = require('../models/user.model');
 const formValidator = require('../scripts/FormValidator');
@@ -46,3 +47,90 @@ test('expect updatePassword send update password request even if old password do
         expect(userModel.updateCurrentPassword.mock.calls.length).toBe(1);
     }
 )
+
+test('expect activity type list to be updated',
+    async () => {
+        userModel.addUserActivityType = jest.fn();
+        userModel.getCurrentUser = jest.fn(() => {
+            var user = {"activities": []} as UserApiFormat;
+            return user;
+        });
+
+        await addActivityType("running");
+        expect(userModel.addUserActivityType.mock.calls[0][0]).toBe("running");
+    }
+)
+
+test('expect no user found when updating activity types with null user',
+    async () => {
+        userModel.addUserActivityType = jest.fn();
+        userModel.getCurrentUser = jest.fn(() => {return null});
+
+        //expect(await addActivityType("running")).toThrowError("No active user found.");
+
+        try {
+            await addActivityType("running")
+            expect(1).toEqual(2);
+        } catch (error) {
+            expect(error.message).toEqual("No active user found.");
+        }
+    })
+
+    test('expect no user found when deleting activity types from null user',
+    async () => {
+        userModel.removeUserActivityType = jest.fn();
+        userModel.getCurrentUser = jest.fn(() => {return null});
+
+        //expect(await addActivityType("running")).toThrowError("No active user found.");
+
+        try {
+            await removeActivityType("running")
+            expect(1).toEqual(2);
+        } catch (error) {
+            expect(error.message).toEqual("No active user found.");
+        }
+    })
+
+test('expect no activity type error when user has no activity type property',
+async () => {
+    userModel.removeUserActivityType = jest.fn();
+    userModel.getCurrentUser = jest.fn(() => {
+        var user = {} as UserApiFormat;
+        return user;
+    });
+
+    try {
+        await removeActivityType("running")
+        expect(1).toEqual(2);
+    } catch (error) {
+        expect(error.message).toEqual("User has no activity types associated with their profile.");
+    }
+})
+
+test('expect no activity type error when user has activity type list empty',
+async () => {
+    userModel.removeUserActivityType = jest.fn();
+    userModel.getCurrentUser = jest.fn(() => {
+        var user = {"activities": []} as UserApiFormat;
+        return user;
+    });
+
+    try {
+        await removeActivityType("running")
+        expect(1).toEqual(2);
+    } catch (error) {
+        expect(error.message).toEqual("User has no activity types associated with their profile.");
+    }
+})
+
+test('expect activity type to be removed from user when exists in list',
+async () => {
+    userModel.removeUserActivityType = jest.fn();
+    userModel.getCurrentUser = jest.fn(() => {
+        var user = {"activities": ["running"]} as UserApiFormat;
+        return user;
+    });
+
+    await removeActivityType("running");
+    await expect(userModel.removeUserActivityType.mock.calls[0][0]).toBe("running");
+})
