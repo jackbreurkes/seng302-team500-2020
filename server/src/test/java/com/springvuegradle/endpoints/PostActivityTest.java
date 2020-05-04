556170ad259f3a9adf54580bf089c057cb16cdd8
Jack van Heugten Breurkes <jack.breurkes@gmail.com>
2020-05-04 21:03:02 +1200

    attempted to implement PostActivityTest. failed.
    #story[246]

--- server/src/test/java/com/springvuegradle/endpoints/PostActivityTest.java
+++ server/src/test/java/com/springvuegradle/endpoints/PostActivityTest.java
@@ -3,64 +3,81 @@ package com.springvuegradle.endpoints;
 import com.springvuegradle.exceptions.InvalidRequestFieldException;
 import com.springvuegradle.exceptions.RecordNotFoundException;
 import com.springvuegradle.model.data.Activity;
+import com.springvuegradle.model.data.Profile;
+import com.springvuegradle.model.repository.ActivityRepository;
+import com.springvuegradle.model.repository.ActivityTypeRepository;
+import com.springvuegradle.model.repository.ProfileRepository;
+import com.springvuegradle.model.repository.UserRepository;
 import com.springvuegradle.model.requests.CreateActivityRequest;
+import com.springvuegradle.model.responses.ProfileResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.test.mock.mockito.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@WebMvcTest(ActivitiesController.class)
+@Disabled
 class PostActivityTest {
 
-//    private ActivitiesController activitiesController;
-//    private CreateActivityRequest createActivityRequest;
-//
-//    @BeforeEach
-//    public void beforeEach() {
-//        activitiesController = new ActivitiesController();
-//
-//        createActivityRequest = new CreateActivityRequest();
-//        createActivityRequest.setActivityName("Kaikoura Coast Track race");
-//        createActivityRequest.setDescription("A big and nice race on a lovely peninsula");
-//        createActivityRequest.setActivityTypes(Arrays.asList("Walking", "Running"));
-//        createActivityRequest.setContinuous(false);
-//        createActivityRequest.setStartTime("2020-02-20T08:00:00+1300");
-//        createActivityRequest.setEndTime("2020-02-20T08:00:00+1300");
-//        createActivityRequest.setLocation("Kaikoura, NZ");
-//    }
-//
-//    @Test
-//    public void testCreateActivity() throws InvalidRequestFieldException, RecordNotFoundException {
-//        String activityName = createActivityRequest.getActivityName();
-//        Activity createdActivity = activitiesController.createActivity(1L, createActivityRequest);
-//        assertEquals(activityName, createdActivity.getActivityName());
-//    }
-//
-//    @Test
-//    public void testInvalidRequestErrorIfNameMissing() {
-//        createActivityRequest.setActivityName(null);
-//        assertThrows(InvalidRequestFieldException.class, () -> {
-//            activitiesController.createActivity(1L, createActivityRequest);
-//        });
-//    }
-//
-//    @Test
-//    public void testInvalidRequestErrorIfDurationAndNoEndTime() {
-//        createActivityRequest.setContinuous(false);
-//        createActivityRequest.setEndTime(null);
-//        assertThrows(InvalidRequestFieldException.class, () -> {
-//            activitiesController.createActivity(1L, createActivityRequest);
-//        });
-//    }
-//
-//    @Test
-//    public void testErrorIfNameUnderFourCharacters() {
-//        createActivityRequest.setActivityName("tes");
-//        assertThrows(MethodArgumentNotValidException.class, () -> {
-//            activitiesController.createActivity(1L, createActivityRequest);
-//        });
-//    }
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ActivityRepository activityRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private ProfileRepository profileRepository;
+
+    @MockBean
+    private ActivityTypeRepository activityTypeRepository;
+
+
+    private ActivitiesController activitiesController;
+    private CreateActivityRequest createActivityRequest;
+
+    @BeforeEach
+    public void beforeEach() {
+        activitiesController = new ActivitiesController();
+
+        createActivityRequest = new CreateActivityRequest();
+        createActivityRequest.setActivityName("Kaikoura Coast Track race");
+        createActivityRequest.setDescription("A big and nice race on a lovely peninsula");
+        createActivityRequest.setActivityTypes(Arrays.asList("Walking", "Running"));
+        createActivityRequest.setContinuous(false);
+        createActivityRequest.setStartTime("2020-02-20T08:00:00+1300");
+        createActivityRequest.setEndTime("2020-02-20T08:00:00+1300");
+        createActivityRequest.setLocation("Kaikoura, NZ");
+    }
+
+    @Test
+    public void testExample() throws Exception {
+        this.mvc.perform(post("/profiles/1/activities", createActivityRequest).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+//                .andExpect(content().)
+    }
+
 
 }
