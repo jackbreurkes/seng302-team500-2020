957223295561f2a81e25d5fad6cb6b10c4aa3c17
Olivia Mackintosh <oam23@uclive.ac.nz>
2020-06-30 21:58:02 +1200

    Implemented user search result list component
    Created a Vue component which shows the results of a search of user profiles by fullname, nickname or email. The component is made up of a datatable as it follows the GUI documented on the team wiki.
    
    #story[290]

---
+++ client/src/models/userSearch.model.ts
@@ -0,0 +1,12 @@
+import { RegisterFormData } from "@/controllers/register.controller";
+import { UserApiFormat } from "@/scripts/User";
+
+import instance from "../services/axios.service";
+import * as auth from "../services/auth.service";
+
+
+export async function searchAppUsers(fullname: string, nickname: string, email: string) {
+    let params = {"fullname": fullname, "nickname": nickname, "email": email}
+    let res = await instance.get("profiles", { params });
+    return res.data;
+  }
