3e0ed87a9df03ac65f9f47b1e2820367c8386232
Joshua Yee <jmy39@uclive.ac.nz>
2020-07-17 18:21:33 +1200

    Automated tests complete for searching by email on the repository
    #story[290] #test

--- server/src/test/java/com/springvuegradle/model/repository/EmailRepositoryTest.java
+++ server/src/test/java/com/springvuegradle/model/repository/EmailRepositoryTest.java
@@ -1,69 +1,125 @@
-//package com.springvuegradle.model.repository;
-//
-//import com.springvuegradle.model.data.*;
-//import org.junit.jupiter.api.*;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvSource;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import java.time.LocalDate;
-//import java.util.*;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//public class EmailRepositoryTest {
-//
-//
-//    @Autowired
-//    EmailRepository emailRepository;
-//
-//    Email email1;
-//    Email email2;
-//    Email email3;
-//    Email email4;
-//    Email email5;
-//    Email email6;
-//
-//    @BeforeAll
-//    public void createTestProfiles() {
-//        email1 = new Email(new User(1), "primary@test.com", true);
-//        email2 = new Email(new User(2), "primary@1234.com", true);
-//        email3 = new Email(new User(3), "secondary@test.com", false);
-//        email4 = new Email(new User(4), "josh@josh.com", true);
-//        email5 = new Email(new User(5), "olivia@olivia.com", false);
-//        email6 = new Email(new User(6), "olivia@double.com", false);
-//
-//
-//        emailRepository.save(email1);
-//        emailRepository.save(email2);
-//        emailRepository.save(email3);
-//        emailRepository.save(email4);
-//        emailRepository.save(email5);
-//        emailRepository.save(email6);
-//
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({
-//            "primary@test.com,1",
-//            "primary@1234.com,2",
-//            "secondary@test.com,3",
-//            "josh@josh.com,4",
-//            "olivia@olivia.com,1",
-//            "olivia@double.com,1" // duplicate terms
-//    })
-//
-//    public void retrieveSingleUserWithValidEmail(String input) {
-//        String[] splitString = input.split(",");
-//        String email = splitString[0];
-//        int userId = Integer.parseInt(splitString[1]);
-//        List<Email> result = emailRepository.findByEmailStartingWith(email);
-//        assertEquals(userId, result.get(0).getUser().getUserId());
-//    }
-//}
+package com.springvuegradle.model.repository;
+
+import com.springvuegradle.model.data.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class EmailRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    Email email1;
+    Email email2;
+    Email email3;
+    Email email4;
+    Email email5;
+    Email email6;
+
+    @BeforeAll
+    public void createTestProfiles() {
+        //create users
+        User user1 = new User(1);
+        User user2 = new User(2);
+        User user3 = new User(3);
+        User user4 = new User(4);
+//        User user5 = new User(5);
+        //save users
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+        userRepository.save(user4);
+
+        //create emails and assign them to users
+        email1 = new Email(user1, "primary@test.com", true);
+        email2 = new Email(user2, "primary@1234.com", true);
+        email3 = new Email(user3, "secondary@test.com", false);
+        email4 = new Email(user4, "josh@josh.com", true);
+        email5 = new Email(user1, "olivia@olivia.com", false);
+        email6 = new Email(user1, "olivia@double.com", false);
+
+        //save users
+        emailRepository.save(email1);
+        emailRepository.save(email2);
+        emailRepository.save(email3);
+        emailRepository.save(email4);
+        emailRepository.save(email5);
+        emailRepository.save(email6);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "primary@test.com,1",
+            "primary@1234.com,2",
+            "secondary@test.com,3",
+            "josh@josh.com,4",
+            "olivia@olivia.com,1",
+            "olivia@double.com,1" // duplicate terms
+    })
+    public void retrieveSingleUserWithValidFullEmail(String email, String userId) {
+        int userIdNum = Integer.parseInt(userId);
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(userIdNum, result.get(0).getUser().getUserId());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "josh,4",
+            "olivia@,1",
+            "olivia,1" // duplicate terms
+    })
+    public void retrieveSingleUserWithValidPartialEmail(String email, String userId) {
+        int userIdNum = Integer.parseInt(userId);
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(userIdNum, result.get(0).getUser().getUserId());
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "primary,2",
+            "primary@,2",
+            "primary@1234.com,2",
+            "primary@test.com,1",
+            //"primary,1" TODO is broken
+
+    })
+    public void retrieveMultipleUsersWithIdenticalValidPartialEmail(String email, String userId) {
+        int userIdNum = Integer.parseInt(userId);
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(userIdNum, result.get(0).getUser().getUserId());
+    }
+
+    @Test
+    public void retrieveSingleUserWithInvalidFullEmail() {
+        String email = "thisdoesntexist@repository.com";
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(0, result.size());
+    }
+    @Test
+    public void retrieveSingleUserWithInvalidPartialEmail() {
+        String email = "thisdoesntexist";
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(0, result.size());
+    }
+    @Test
+    public void retrieveSingleUserWithInvalidEmptyEmail() {
+        String email = " ";
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(0, result.size());
+    }
+}
