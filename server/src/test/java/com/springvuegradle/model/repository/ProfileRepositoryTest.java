72ce515de098d5cdb42e0712ae242e0703fe3598
Joshua Yee <jmy39@uclive.ac.nz>
2020-07-17 13:47:17 +1200

    starting some tests

--- server/src/test/java/com/springvuegradle/model/repository/ProfileRepositoryTest.java
+++ server/src/test/java/com/springvuegradle/model/repository/ProfileRepositoryTest.java
@@ -23,7 +23,13 @@ public class ProfileRepositoryTest {
     ProfileRepository profileRepository;
 
     @Autowired
+    UserRepository userRepository;
+
+    @Autowired
     ActivityTypeRepository activityTypeRepository;
+
+    @Autowired
+    EmailRepository emailRepository;
 
     @BeforeAll
     public void createTestProfiles() {
@@ -39,15 +45,46 @@ public class ProfileRepositoryTest {
         activityTypeRepository.save(skiing);
         activityTypeRepository.save(tramping);
         activityTypeRepository.save(scootering);
+        User user1 = new User(1);
+        user1 = userRepository.save(user1);
+        User user2 = new User(2);
+        user2 = userRepository.save(user2);
+        User user3 = new User(3);
+        user3 = userRepository.save(user3);
+        User user4 = new User(4);
+        user4 = userRepository.save(user4);
+        User user5 = new User(5);
+        user5 = userRepository.save(user5);
+//        //userRepository.save(user1);
+//        //userRepository.save(user2);
+//        userRepository.save(user3);
+//        userRepository.save(user4);
+//        userRepository.save(user5);
+        Profile profile1 = new Profile(user1, "Bill", "Testman", LocalDate.EPOCH, Gender.NON_BINARY);
+        Profile profile2 = new Profile(user2, "Andy", "Warshaw", LocalDate.EPOCH, Gender.NON_BINARY);
+        Profile profile3 = new Profile(user3, "Brie", "Calaris", LocalDate.EPOCH, Gender.NON_BINARY);
+        Profile profile4 = new Profile(user4, "Jack", "Sandler", LocalDate.EPOCH, Gender.NON_BINARY);
+        Profile profile5 = new Profile(user5, "Jill", "Sandler", LocalDate.EPOCH, Gender.NON_BINARY);
+        Email email1 = new Email(user1, "primary@test.com", true);
+        Email email2 = new Email(user2, "primary@1234.com", true);
+        Email email3 = new Email(user1, "secondary@test.com", false);
+        Email email4 = new Email(user3, "josh@josh.com", true);
+        Email email5 = new Email(user1, "olivia@olivia.com", false);
+        Email email6 = new Email(user1, "olivia@double.com", false);
 
-        Profile profile1 = new Profile(new User(), "Bill", "Testman", LocalDate.EPOCH, Gender.NON_BINARY);
-        Profile profile2 = new Profile(new User(), "Andy", "Warshaw", LocalDate.EPOCH, Gender.NON_BINARY);
-        Profile profile3 = new Profile(new User(), "Brie", "Calaris", LocalDate.EPOCH, Gender.NON_BINARY);
-        Profile profile4 = new Profile(new User(), "Jack", "Sandler", LocalDate.EPOCH, Gender.NON_BINARY);
-        Profile profile5 = new Profile(new User(), "Jill", "Sandler", LocalDate.EPOCH, Gender.NON_BINARY);
+
+
+        emailRepository.save(email1);
+        emailRepository.save(email2);
+        emailRepository.save(email3);
+        emailRepository.save(email4);
+        emailRepository.save(email5);
+        emailRepository.save(email6);
+
         profile1.setActivityTypes(Arrays.asList(
                 walking
         ));
+
         profile2.setActivityTypes(Arrays.asList(
                 walking, running
         ));
@@ -130,4 +167,22 @@ public class ProfileRepositoryTest {
         assertEquals(0, result.size());
     }
 
+
+    //EMAIL STUFF
+    @ParameterizedTest
+    @CsvSource({
+            "primary@test.com,1",
+            "primary@1234.com,2",
+            "secondary@test.com,3",
+            "josh@josh.com,4",
+            "olivia@olivia.com,1",
+            "olivia@double.com,1" // duplicate terms
+    })
+    public void retrieveSingleUserWithValidEmail(String input) {
+        String[] splitString = input.split(",");
+        String email = splitString[0];
+        int userId = Integer.parseInt(splitString[1]);
+        List<Email> result = emailRepository.findByEmailStartingWith(email);
+        assertEquals(userId, result.get(0).getUser().getUserId());
+    }
 }
