4c7a155ca55eded6508dd069795459d1b13c163f
Michael Freeman <michaelfreeman100@gmail.com>
2020-07-29 10:40:48 +1200

    Re-added Roles class which is used for admin permissions and changed enum to ActivityRole

--- server/src/main/java/com/springvuegradle/model/data/Role.java
+++ server/src/main/java/com/springvuegradle/model/data/Role.java
@@ -1,22 +1,60 @@
 package com.springvuegradle.model.data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 /**
- * Role Enum
- * For implementing user's roles in relation to activities
+ * JPA POJO representing a Role.
  */
-public enum Role {
-    CREATOR("creator"), ORGANISER("organiser"), PARTICIPANT("participant"), FOLLOWER("follower");
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @NotNull
+    private long role_id;
+
+    // the @NotNull annotation will automatically set the column to not null
+    // if hibernate.validator.apply_to_ddl = true (true by default)
+
+    @NotNull
+    @Column(columnDefinition = "varchar(30)")
+    private String rolename;
+
 
     /**
-     * Json name for the Role
+     * no arg constructor required by JPA
      */
-    private final String jsonName;
+    public Role() {}
 
     /**
-     * Constructor for Role
-     * @param jsonName the name for the role given in the json request
+     * Create a role with required id and name fields
+     * @param id the id of the role
+     * @param rolename the name of the role
      */
-    private Role(String jsonName){
-        this.jsonName = jsonName;
+    public Role(long id, String rolename)
+    {
+        this.role_id = id;
+        this.rolename = rolename;
     }
+
+    public long getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(long role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+
 }
