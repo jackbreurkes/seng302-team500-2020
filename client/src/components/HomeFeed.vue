e42fcd28e35101a948d4fdbb0c9e48776f7bcb59
James <jau24@uclive.ac.nz>
2020-07-29 11:15:18 +1200

    Added blank homepage with burger menu navigation
    Still requires functionality of notification card to be done on the backend.
    #story[376]

---
+++ client/src/components/HomeFeed.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="Homefeed">
+    <v-layout justify-center class="pt-1">
+        <v-card width='900' height='650'>
+            <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Home Feed</v-toolbar-title>
+            </v-toolbar>
+        </v-card>
+    </v-layout>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+// app Vue instance
+const Homefeed = Vue.extend({
+  name: "Homefeed",
+  el: "#Homefeed",
+
+  // app initial state
+  data: function() {
+    return {
+    };
+  },
+
+  methods: {
+  }
+});
+
+export default Homefeed;
+</script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+
+.mandatory {
+  background-color: #ccf;
+}
+
+p {
+    display: inline-block;
+  }
+</style>
