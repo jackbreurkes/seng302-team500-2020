e9cf62843e39dbf881d07c10655c5bfdd94202ed
Michael Freeman <michaelfreeman100@gmail.com>
2020-07-29 10:23:56 +1200

    Implemented role enum for allowing users to have different roles in relation to activities #implement

--- server/src/main/java/com/springvuegradle/model/data/Role.java
+++ server/src/main/java/com/springvuegradle/model/data/Role.java
@@ -1,60 +1,22 @@
 package com.springvuegradle.model.data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
 /**
- * JPA POJO representing a Role.
+ * Role Enum
+ * For implementing user's roles in relation to activities
  */
-@Entity
-@Table(name = "role")
-public class Role {
-
-    @Id
-    @NotNull
-    private long role_id;
-
-    // the @NotNull annotation will automatically set the column to not null
-    // if hibernate.validator.apply_to_ddl = true (true by default)
-
-    @NotNull
-    @Column(columnDefinition = "varchar(30)")
-    private String rolename;
-
+public enum Role {
+    CREATOR("creator"), ORGANISER("organiser"), PARTICIPANT("participant"), FOLLOWER("follower");
 
     /**
-     * no arg constructor required by JPA
+     * Json name for the Role
      */
-    public Role() {}
+    private final String jsonName;
 
     /**
-     * Create a role with required id and name fields
-     * @param id the id of the role
-     * @param rolename the name of the role
+     * Constructor for Role
+     * @param jsonName the name for the role given in the json request
      */
-    public Role(long id, String rolename)
-    {
-        this.role_id = id;
-        this.rolename = rolename;
+    private Role(String jsonName){
+        this.jsonName = jsonName;
     }
-
-	public long getRole_id() {
-		return role_id;
-	}
-
-	public void setRole_id(long role_id) {
-		this.role_id = role_id;
-	}
-
-	public String getRolename() {
-		return rolename;
-	}
-
-	public void setRolename(String rolename) {
-		this.rolename = rolename;
-	}
-
 }
