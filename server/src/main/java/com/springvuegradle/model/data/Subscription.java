a3bb0ffa31f205c9786e0be486c56e134a98e719
Alex Hobson <aho113@uclive.ac.nz>
2020-07-30 16:32:07 +1200

    Update subscription repository to keep track of start/end date

--- server/src/main/java/com/springvuegradle/model/data/Subscription.java
+++ server/src/main/java/com/springvuegradle/model/data/Subscription.java
@@ -1,5 +1,8 @@
 package com.springvuegradle.model.data;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -20,20 +23,67 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "subscriptions")
 @NamedQuery(name = "Subscription.findSubscriptionsByUser", query = "SELECT s FROM Subscription s WHERE s.subscriber = ?1")
+@NamedQuery(name = "Subscription.findActiveSubscriptionsByUser", query = "SELECT s FROM Subscription s WHERE s.subscriber = ?1 AND s.isCurrentSubscription = true")
 public class Subscription {
 
+	/**
+	 * auto generated unique ID
+	 */
 	@Id
 	@GeneratedValue
 	private long id;
 
+	/**
+	 * The profile this subscription is relevant to
+	 */
 	@ManyToOne
 	@JoinColumn(name="subscriber", nullable=false)
 	private Profile subscriber;
 	
+	/**
+	 * The type of entity that entityId represents
+	 */
 	@Enumerated(EnumType.ORDINAL)
 	private HomefeedEntityType entityType;
 	
+	/**
+	 * The unique ID of the subscribed entity 
+	 */
 	private long entityId;
+
+	/**
+	 * true if the subscription hasn't ended
+	 */
+	private boolean isCurrentSubscription;
+	
+	@Column(nullable = false)
+	private LocalDateTime subscribedDate;
+	
+	private LocalDateTime unsubscribedDate;
+
+	/**
+	 * Default JPA constructor
+	 */
+	public Subscription() {
+		subscribedDate = LocalDateTime.now();
+	}
+	
+	/**
+	 * Create an object representing when a user subscribed to something
+	 * This constructor will set the subscribed date to the current system time,
+	 * you can call {@link Subscription#setSubscribedDate(LocalDateTime)} to
+	 * override this time
+	 * 
+	 * @param subscriber Who this subscription represents
+	 * @param entityType The type of entity this subscription represents
+	 * @param entityId The unique ID of the entity this subscription represents
+	 */
+	public Subscription(Profile subscriber, HomefeedEntityType entityType, long entityId) {
+		this.subscriber = subscriber;
+		this.entityType = entityType;
+		this.entityId = entityId;
+		subscribedDate = LocalDateTime.now();
+	}
 
 	public long getId() {
 		return id;
@@ -65,5 +115,38 @@ public class Subscription {
 
 	public void setEntityId(long entityId) {
 		this.entityId = entityId;
+	}
+	
+	public LocalDateTime getSubscribedDate() {
+		return subscribedDate;
+	}
+
+	public void setSubscribedDate(LocalDateTime subscribedDate) {
+		this.subscribedDate = subscribedDate;
+	}
+
+	public LocalDateTime getUnsubscribedDate() {
+		return unsubscribedDate;
+	}
+
+	public void setUnsubscribedDate(LocalDateTime unsubscribedDate) {
+		this.unsubscribedDate = unsubscribedDate;
+	}
+	
+	/**
+	 * Marks this subscription as ended and sets the end date to the
+	 * current system time
+	 */
+	public void unsubscribe() {
+		this.setCurrentSubscription(false);
+		this.setUnsubscribedDate(LocalDateTime.now());
+	}
+	
+	public boolean isCurrentSubscription() {
+		return isCurrentSubscription;
+	}
+
+	public void setCurrentSubscription(boolean isCurrentSubscription) {
+		this.isCurrentSubscription = isCurrentSubscription;
 	}
 }
--- server/src/main/java/com/springvuegradle/model/repository/SubscriptionRepository.java
+++ server/src/main/java/com/springvuegradle/model/repository/SubscriptionRepository.java
@@ -7,7 +7,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springvuegradle.model.data.Profile;
 import com.springvuegradle.model.data.Subscription;
 
+/**
+ * JPA repository for subscriptions
+ * @author Alex Hobson
+ *
+ */
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
+	/**
+	 * Finds all the subscriptions relevant to a given user including
+	 * instances where they have unsubscribed
+	 * @param profile Profile to find subscriptions of
+	 * @return List of subscriptions (may be empty)
+	 */
 	public List<Subscription> findSubscriptionsByUser(Profile profile);
+	
+	/**
+	 * Finds all the active subscriptions relevant to a given user
+	 * excluding instances where they have unsubscribed
+	 * @param profile Profile to find subscriptions of
+	 * @return List of subscriptions (may be empty)
+	 */
+	public List<Subscription> findActiveSubscriptionsByUser(Profile profile);
 }
