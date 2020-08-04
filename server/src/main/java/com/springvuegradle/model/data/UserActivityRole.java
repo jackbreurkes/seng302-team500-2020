21aacb49c5eb46429799926116f3762a12a7547b
Michael Morgoun <mmo155@uclive.ac.nz>
2020-08-05 08:10:34 +1200

    Added javadoc to the Activity Role JPA

--- server/src/main/java/com/springvuegradle/model/data/UserActivityRole.java
+++ server/src/main/java/com/springvuegradle/model/data/UserActivityRole.java
@@ -7,25 +7,45 @@ import javax.validation.constraints.NotNull;
 @Entity
 public class UserActivityRole {
 
+    /**
+     * A generated ID identifier for each user-activity-role relationship
+     */
     @NotNull
     @GeneratedValue
     @Id
     private long userActivityRoleId;
 
+    /**
+     * The ID of the activity the user is a part of
+     */
     @OneToOne
     @JoinColumn(columnDefinition = "activity_id")
     private Activity activity;
 
+    /**
+     * The ID of the user
+     */
     @OneToOne
     @JoinColumn(columnDefinition = "uuid")
     private User user;
 
+    /**
+     * The specific role of a user in an activity
+     */
     @NotNull
     private ActivityRole activityRole;
 
-
+    /**
+     * Default JPA Constructor
+     */
     public UserActivityRole(){}
 
+    /**
+     * Create an object that represents the role a user has for a specific activity
+     * @param activity The ID of the activity the user is a part of
+     * @param user The ID of the user
+     * @param role The new role of a user for an activity
+     */
     public UserActivityRole(Activity activity, User user, ActivityRole role){
         this.activity = activity;
         this.user = user;
