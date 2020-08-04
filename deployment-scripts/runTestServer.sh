50bb27d3a726532b761dd3b5b792953326f017ae
Alex Hobson <aho113@uclive.ac.nz>
2020-08-05 10:37:23 +1200

    Move mariadb statement to application.properties #story[377] #pair[aho113, jsv22]

--- deployment-scripts/runTestServer.sh
+++ deployment-scripts/runTestServer.sh
@@ -1,2 +1,2 @@
 fuser -k 9499/tcp || true
-java -Dspring.profiles.active=dev -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499 --spring.datasource.url=jdbc:mariadb://DB_URL --spring.datasource.username=DB_USERNAME --spring.datasource.password=DB_PASSWORD  --spring.jpa.database-platform=org.hibernate.dialect.MariaDB103Dialect
+java -Dspring.profiles.active=dev -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499 --spring.datasource.url=jdbc:mariadb://DB_URL --spring.datasource.username=DB_USERNAME --spring.datasource.password=DB_PASSWORD
