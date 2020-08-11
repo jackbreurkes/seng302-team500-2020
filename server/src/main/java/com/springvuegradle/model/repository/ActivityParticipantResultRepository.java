1406c34950ef1226a6eeb8854d8e8f177265dc7d
Alex Hobson <aho113@uclive.ac.nz>
2020-08-11 15:24:01 +1200

    Implement Activity participant results as POJO classes #story[390]

---
+++ server/src/main/java/com/springvuegradle/model/repository/ActivityParticipantResultRepository.java
@@ -0,0 +1,12 @@
+package com.springvuegradle.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springvuegradle.model.data.ActivityParticipantResult;
+
+/**
+ * Repository for ActivityParticipantResults
+ */
+public interface ActivityParticipantResultRepository extends JpaRepository<ActivityParticipantResult, Long>{
+
+}
