2507ca39e82de3c764ad7b23da0cfedfd3d8f6df
Michael Freeman <michaelfreeman100@gmail.com>
2020-08-15 12:36:40 +1200

    Implemented homefeedcards on homefeed, correct messages displaying for each different changelog event #implement

--- client/src/components/HomeFeedCard.vue
+++ client/src/components/HomeFeedCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="HomeFeedCard">
     <v-layout justify-center class="pt-1">
-        <v-card width='600' height='150'>
+        <v-card width='600' height='100%'>
             <v-toolbar @click=activityClicked color="blue" dark flat height='50'>
                 <v-card-title>{{activityName}}</v-card-title>
             </v-toolbar>
-            <v-toolbar-title class="pl-7 pt-3" @click=creatorClicked>Created by {{creatorName}}</v-toolbar-title>
+            <v-card-text class="pl-7 pt-3 " @click=creatorClicked>Created by {{creatorName}}</v-card-text>
                 <v-spacer></v-spacer>
-            <v-card-text class="pl-7">{{userActionName}}, performed {{userAction}} on {{userActionTimeStamp}}</v-card-text>
+            <v-card-text class="pl-7 subtitle-1">{{infoString}}</v-card-text>
         </v-card>
     </v-layout>
   </div>
@@ -23,15 +23,19 @@ const HomeFeedCard = Vue.extend({
         return{
             activityName: this.cardData.entity_name,
             creatorName: this.cardData.creator_name,
-            userActionName: this.cardData.editor_action,
-            userAction: this.cardData.editor_action,
-            userActionTimeStamp: "01/01/1970; 24:70",
+            editorName: this.cardData.editorName,
+            editorAction: "edited",
+            userActionTimeStamp: this.cardData.edited_timeStamp,
             entity_id: this.cardData.entity_id,
-            creator_id: this.cardData.creator_id
+            creator_id: this.cardData.creator_id,
+            oldValue: "",
+            newValue: "",
+            infoString: ""
         };
     },
     created(){
-
+        this.parseChangelogResponse();
+        
     },
 
     methods: {
@@ -40,6 +44,45 @@ const HomeFeedCard = Vue.extend({
         },
         creatorClicked: function(){
             this.$router.push("/profiles/" + this.creator_id)
+        },
+        parseTime: function(time:string){
+            let dateTime = new Date(time);
+            const dtf = new Intl.DateTimeFormat(undefined, {
+                year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short'});
+            return dtf.format(dateTime)
+        },
+        parseEditorAction: function(){
+            if(this.cardData.changed_attribute == "ACTIVITY_EXISTENCE"){
+                return "created the activity " + this.cardData.entity_name;
+            }else if(this.cardData.changed_attribute == "ACTIVITY_NAME"){
+                return "updated the activity name from \"" + this.cardData.old_value + "\" to \"" + this.cardData.new_value + "\"";
+            }else if(this.cardData.changed_attribute == "ACTIVITY_DESCRIPTION"){
+                return "updated the activity description from \"" + this.cardData.old_value + "\" to \"" + this.cardData.new_value + "\"";
+            }else if(this.cardData.changed_attribute == "ACTIVITY_TIME_FRAME"){
+                if(this.cardData.old_value == null){
+                    //A timeframe has been added where there was none
+                    return "added a time to the activity, now running from " + this.parseTime(this.cardData.new_value.start_time) + " to " +this.parseTime(this.cardData.new_value.end_time) + ". Changed"
+                }else{
+                    return "updated the activities time. Used to run from " + this.parseTime(this.cardData.old_value.start_time) + " to " + this.parseTime(this.cardData.old_value.end_time) + ". Now runs from "
+                     + this.parseTime(this.cardData.new_value.start_time) + " to " +this.parseTime(this.cardData.new_value.end_time) + ". Changed";
+                }
+            }else if(this.cardData.changed_attribute == "ACTIVITY_LOCATION"){
+                return "updated the activity location from " + this.cardData.old_value + " to " + this.cardData.new_value;
+            }else if(this.cardData.changed_attribute == "ACTIVITY_ACTIVITY_TYPES"){
+                var activityTypesString =  "updated the activities activity types from ";
+                this.cardData.old_value.forEach((element:string) => {
+                    activityTypesString += element + ", "
+                });
+                activityTypesString = activityTypesString.substring(0, activityTypesString.length - 2) + " to "
+                this.cardData.new_value.forEach((element:string) => {
+                    activityTypesString += element + ", ";
+                });
+                return activityTypesString.substring(0, activityTypesString.length - 2);
+            }
+            return this.cardData.changed_attribute;
+        },
+        parseChangelogResponse: function(){
+            this.infoString = this.cardData.editor_name + " " + this.parseEditorAction() + " on " + this.parseTime(this.cardData.edited_timestamp);
         }
     }
 })
--- client/src/scripts/HomeFeedCardType.ts
+++ client/src/scripts/HomeFeedCardType.ts
@@ -3,8 +3,10 @@ export interface HomeFeedCardType {
     entitiy_name: string,
     creator_id: number,
     creator_name: string,
-    edited_timeStamp: string,
+    edited_timestamp: string,
     editor_id: number,
     editor_name: string,
-    editor_action: string
+    changed_attribute: string,
+    old_value: string,
+    new_value: JSON
 }
