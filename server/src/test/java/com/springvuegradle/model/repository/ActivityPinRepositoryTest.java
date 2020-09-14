947766028366377845938e2f778b5786ca1badc5
Jack van Heugten Breurkes <jsv22@uclive.ac.nz>
2020-09-15 10:12:47 +1200

    paginated findPinsInBound query
    #story[414] #pair[jsv22, aho113]

--- server/src/test/java/com/springvuegradle/model/repository/ActivityPinRepositoryTest.java
+++ server/src/test/java/com/springvuegradle/model/repository/ActivityPinRepositoryTest.java
@@ -5,20 +5,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -69,35 +68,35 @@ public class ActivityPinRepositoryTest {
     @Test
     void topLeftCorner_expectNone(){
         generateActivityAndPin("Top Left", 0, 1);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertTrue(activityPins.isEmpty());
     }
 
     @Test
     void bottomLeftCorner_exepctNone(){
         generateActivityAndPin("Bottom Left", 0, 0);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertTrue(activityPins.isEmpty());
     }
 
     @Test
     void bottomRightCorner_expectNone(){
         generateActivityAndPin("Bottom Right", 1, 0);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertTrue(activityPins.isEmpty());
     }
 
     @Test
     void topRightCorner_expectOne(){
         generateActivityAndPin("Top Right", 1, 1);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertEquals(1, activityPins.size());
     }
 
     @Test
     void middle_expectOne(){
         generateActivityAndPin("Middle", 0.5f, 0.5f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertEquals(1, activityPins.size());
     }
 
@@ -105,7 +104,7 @@ public class ActivityPinRepositoryTest {
     void middle_expectTwo(){
         generateActivityAndPin("Middle", 0.5f, 0.5f);
         generateActivityAndPin("Middle2", 0.66f, 0.1f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertEquals(2, activityPins.size());
     }
 
@@ -115,40 +114,40 @@ public class ActivityPinRepositoryTest {
         generateActivityAndPin("Outside2", -0.66f, -0.1f);
         generateActivityAndPin("Outside3", -6f, 2f);
         generateActivityAndPin("Outside4", 1, -0.1f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertTrue(activityPins.isEmpty());
     }
 
     @Test
     void rightEdge_expectOne(){
         generateActivityAndPin("Right Edge", 1, 0.5f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertEquals(1, activityPins.size());
     }
 
     @Test
     void topEdge_expectOne(){
         generateActivityAndPin("Top Edge", 0.5f, 1f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertEquals(1, activityPins.size());
     }
 
     @Test
     void bottomEdge_expectNone(){
         generateActivityAndPin("Bottom Edge", 0.5f, 0f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertTrue(activityPins.isEmpty());
     }
 
     @Test
     void leftEdge_ExpectNone(){
         generateActivityAndPin("Left Edge", 0, 0.5f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(1, 1, 0, 0, Pageable.unpaged());
         assertTrue(activityPins.isEmpty());
     }
 
     @Test
-    void negativeBoundingBox_expectOne() {
+    void negativeBoundingBox_expectThree() {
         generateActivityAndPin("Pin1", -1, -1);
         generateActivityAndPin("Pin2", -0.6f, -1.5f);
         generateActivityAndPin("Pin3", -1, -2.2f);
@@ -156,7 +155,29 @@ public class ActivityPinRepositoryTest {
         generateActivityAndPin("OutsidePin2", 0.3f, 0.5f);
         generateActivityAndPin("OutsidePin3", -2.4f, -1);
         generateActivityAndPin("OutsidePin4", -1, -0.55f);
-        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(-0.5f, -0.6f, -2, -2.5f);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(-0.5f, -0.6f, -2, -2.5f, Pageable.unpaged());
         assertEquals(3, activityPins.size());
     }
+
+    @Test
+    void testSouthwestLessThanNortheast_expectNone() {
+        generateActivityAndPin("Pin1", 0, 0);
+        generateActivityAndPin("Pin2", -0.5f, -0.5f);
+        generateActivityAndPin("Pin3", 0.5f, 0.5f);
+        List<ActivityPin> activityPins = activityPinRepository.findPinsInBounds(-1, -1, 1, 1, Pageable.unpaged());
+        assertEquals(0, activityPins.size());
+    }
+
+    @Test
+    void paginatedBoundingBox_twoPerPage_expectTwoPages() {
+        generateActivityAndPin("Pin1", 0.5f, 0.5f);
+        generateActivityAndPin("Pin2", 0.6f, 0.6f);
+        generateActivityAndPin("Pin3", 0.4f, 0.4f);
+        List<ActivityPin> activityPinsPageOne = activityPinRepository.findPinsInBounds(1, 1, 0, 0, PageRequest.of(0, 2));
+        List<ActivityPin> activityPinsPageTwo = activityPinRepository.findPinsInBounds(1, 1, 0, 0, PageRequest.of(1, 2));
+        assertEquals(2, activityPinsPageOne.size());
+        assertEquals(1, activityPinsPageTwo.size());
+    }
+
+
 }
