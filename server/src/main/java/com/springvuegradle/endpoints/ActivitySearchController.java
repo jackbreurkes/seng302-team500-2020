fc3ec68e869853ae2b37e11e52267f37343a6b69
Michael Freeman <michaelfreeman100@gmail.com>
2020-09-15 11:30:21 +1200

    Added pagination to the backend endpoint for activity search and changed the endpoint to use queries instead of body #implement #test

--- server/src/main/java/com/springvuegradle/endpoints/ActivitySearchController.java
+++ server/src/main/java/com/springvuegradle/endpoints/ActivitySearchController.java
@@ -6,16 +6,13 @@ import com.springvuegradle.exceptions.RecordNotFoundException;
 import com.springvuegradle.exceptions.UserNotAuthenticatedException;
 import com.springvuegradle.model.data.Activity;
 import com.springvuegradle.model.repository.ActivityRepository;
-import com.springvuegradle.model.requests.CreateActivityRequest;
-import com.springvuegradle.model.requests.SearchActivityRequest;
 import com.springvuegradle.model.responses.ActivityResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -27,8 +24,9 @@ public class ActivitySearchController {
 
     /**
      * Endpoint for searching activities by name
-     * @param searchActivityRequest the list of search terms in request format
      * @param request the request
+     * @param requestSearchTerms the search terms being requested
+     * @param page the page number to return
      * @return List of activity response objects
      * @throws UserNotAuthenticatedException
      * @throws InvalidRequestFieldException
@@ -36,24 +34,39 @@ public class ActivitySearchController {
      */
     @GetMapping
     @CrossOrigin
-    public List<ActivityResponse> searchActivities(@Valid @RequestBody SearchActivityRequest searchActivityRequest, HttpServletRequest request) throws UserNotAuthenticatedException, InvalidRequestFieldException, RecordNotFoundException {
+    public List<ActivityResponse> searchActivities(@RequestParam(value="searchTerms[]")String[] requestSearchTerms, @RequestParam(value="page") int page, HttpServletRequest request) throws UserNotAuthenticatedException, InvalidRequestFieldException, RecordNotFoundException {
 
         UserAuthorizer.getInstance().checkIsAuthenticated(request);
-        //Expecting frontend to strip/split strings appropriate, for both quotation marks and space separated
-        List<String> searchTerms = searchActivityRequest.getSearchTerms();
+        ArrayList<String> searchTerms = new ArrayList<>(Arrays.asList(requestSearchTerms));
+        //Expecting frontend to strip/split strings appropriate, for both quotation marks and space separated. Pages start at 0
         if(searchTerms.size() == 0){
             throw new InvalidRequestFieldException("No Search Terms Entered");
         }
         List<ActivityResponse> searchResults = new ArrayList<>();
+        List<ActivityResponse> highPrioritySearchResults = new ArrayList<>();
         for(String term : searchTerms){
             List<Activity> activitySearchResults = activityRepository.findActivitiesByActivityNameContaining(term);
             for(Activity activity : activitySearchResults){
+                ActivityResponse activityResponse = new ActivityResponse(activity);
                 searchResults.add(new ActivityResponse(activity));
             }
         }
         if(searchResults.size() == 0){
             throw new RecordNotFoundException("No Activities Found");
+        }else if(searchResults.size() <= 25){
+            //Return everything as there are not enough results to paginatie
+            return searchResults;
+        }else{
+            //Pagination
+            int startIndex = 25 * page;
+            if(startIndex >= searchResults.size()){
+                throw new InvalidRequestFieldException("Page index out of range");
+            }
+            int endIndex = startIndex + 25;
+            if(searchResults.size() < endIndex){
+                endIndex = searchResults.size();
+            }
+            return searchResults.subList(startIndex, endIndex);
         }
-        return searchResults;
     }
 }
