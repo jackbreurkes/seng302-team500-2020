54611ac492f3e9bef5eb926a7a46a0efdf7d250f
Michael Freeman <michaelfreeman100@gmail.com>
2020-09-17 12:50:00 +1200

    Added new method in homefeedcontroller for getting reccomneded activities and updated homefeedresponse, added a new value to ChangedAttributeEnum #implement #fix #pair[mjf145, jmy39]

--- server/src/test/java/com/springvuegradle/endpoints/HomeFeedControllerTest.java
+++ server/src/test/java/com/springvuegradle/endpoints/HomeFeedControllerTest.java
@@ -44,6 +44,12 @@ public class HomeFeedControllerTest {
     @InjectMocks
     private HomeFeedController homeFeedController;
 
+    @MockBean
+    private ActivityPinRepository activityPinRepostory;
+
+    @MockBean
+    private UserActivityRoleRepository userActivityRoleReporsitory;
+
     @Autowired
     private MockMvc mvc;
 
