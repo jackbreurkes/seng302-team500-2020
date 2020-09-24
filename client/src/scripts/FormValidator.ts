bc96f8da2f56fd16bd4d75b0b1a8340905be97fd
Michael Freeman <michaelfreeman100@gmail.com>
2020-09-25 11:18:04 +1200

    Fixed error string for DOB in register page #fix

--- client/src/scripts/FormValidator.ts
+++ client/src/scripts/FormValidator.ts
@@ -1,12 +1,12 @@
 import { UserApiFormat } from './User';
 export default class FormValidator {
-    MIN_DATE = '1900-01-01';
+    MIN_DATE = '01-01-1900';
     FIRST_NAME_ERROR_STRING = "First name must be between 1 and 30 characters (inclusive) and contain no numbers";
     LAST_NAME_ERROR_STRING = "Last name must be between 1 and 30 characters (inclusive) and contain no numbers";
     MIDDLE_NAME_ERROR_STRING = "Middle name must be between 1 and 30 characters (inclusive) and contain no numbers";
     NICKNAME_ERROR_STRING = "Nickname must be at least 6 characters and contain no whitespace characters";
     BIO_ERROR_STRING = "Bio must be at least 8 characters";
-    DOB_ERROR_STRING = "Date of birth must be before the current date, after " + this.MIN_DATE + " and given in the format yyyy-mm-dd";
+    DOB_ERROR_STRING = "Date of birth must be before the current date, after " + this.MIN_DATE + " and given in the format dd-mm-yyyy";
     GENDER_ERROR_STRING = "Gender must be one of: male, female, or non-binary";
     EMAIL_ERROR_STRING = "Email is not valid";
     PASSWORD_ERROR_STRING = "Password must be at least 8 characters";
