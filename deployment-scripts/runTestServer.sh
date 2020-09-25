c083d1f976cfc689d5302b73d702bc09fc47f052
Jack van Heugten Breurkes <jsv22@uclive.ac.nz>
2020-09-25 14:02:29 +1200

    super admin creds now use env vars
    #story[441] #pair[jsv22, aho113]

--- deployment-scripts/runTestServer.sh
+++ deployment-scripts/runTestServer.sh
@@ -1,2 +1,2 @@
 fuser -k 9499/tcp || true
-java -Dspring.profiles.active=dev -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499 --spring.datasource.url=jdbc:mariadb://DB_URL --spring.datasource.username=DB_USERNAME --spring.datasource.password=DB_PASSWORD
+java -Dspring.profiles.active=dev -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499 --spring.datasource.url=jdbc:mariadb://DB_URL --spring.datasource.username=DB_USERNAME --spring.datasource.password=DB_PASSWORD --super_admin_email=SUPER_ADMIN_EMAIL --super_admin_password=SUPER_ADMIN_PWD
