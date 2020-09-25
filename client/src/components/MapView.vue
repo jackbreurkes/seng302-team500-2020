bb406485ee37e6532b1e9ae8be3dab55c19f246d
Jack van Heugten Breurkes <jsv22@uclive.ac.nz>
2020-09-25 14:48:11 +1200

    fix error on pin popup after deleting activity
    #story[414] #pair[aho113, jsv22]

--- client/src/components/MapView.vue
+++ client/src/components/MapView.vue
@@ -346,7 +346,12 @@
         this.displayedActivities = [];
         for (let activityIndex in allActivities) {
           let activityId = allActivities[activityIndex];
-          let activity = await getActivityById(activityId.activity_id);
+          let activity;
+          try {
+            activity = await getActivityById(activityId.activity_id);
+          } catch (e) {
+            continue; // activity not loaded, do not attempt to display
+          }
           this.displayedActivities.push(activity);
         }
 
